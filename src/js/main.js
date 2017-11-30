276429fc90638925e0b294c0200b5d16892412b2
Phrenyx <dominikfux@gmx.at>
2017-11-30 23:44:13 +0100

    add sticky icons for image sections

--- src/js/main.js
+++ src/js/main.js
@@ -18,3 +18,19 @@ $('#carrot').click(function() {
         scrollTop: $(window).scrollTop() + $('.image-container-mainpage').height()
     }, 800);
 });
+
+$(window).on( 'scroll', function(){
+    var windowTop = $(window).scrollTop();
+    $( "section.start" ).each(function() {
+    	if(windowTop >= $(this).offset().top){
+      	$(this).find('.piggy-icon').addClass("fixed").removeClass('fixedStart');
+      }else{
+        $(this).find('.piggy-icon').addClass('fixedStart');
+      }
+      if(windowTop >= $(this).offset().top + 350){
+      	$(this).find('.piggy-icon').addClass("fixedBottom");
+      }else{
+        $(this).find('.piggy-icon').removeClass("fixedBottom");
+      }
+    });
+});
