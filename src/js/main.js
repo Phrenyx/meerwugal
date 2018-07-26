db2a8f3f5e7d965c2c9c3f99c74a26bf74a3c087
Phrenyx <dominikfux@gmx.at>
2018-07-26 21:42:51 +0200

    content changes

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,27 @@
 $(window).load(function() {
     $('.preloader').fadeOut();
+
+    if(window.location.hash) {
+        let scroll = $(window.location.hash).offset().top + 0;
+        console.log(window.location.hash);
+        $('html,body').animate({
+            scrollTop: scroll + 'px'
+        },1000,'swing');
+    }
 });
 
 $(document).ready(function() {
+    if (window.location.href.indexOf("sub.html") > -1) {
+        $('a[href*="#"]').on('click',function(e) {
+            e.preventDefault();
+            $('html,body').animate({
+                scrollTop:$($(this).attr('href').replace('/sub.html', '')).offset().top + 'px'
+            },1000,'swing');
+
+            $('.wrapper-menu.open').click();
+        });
+    }
+
   $(".image-grid, .sub").lightGallery({selector: 'a'});
 
   $('.image-container-mainpage').height($(window).height());
